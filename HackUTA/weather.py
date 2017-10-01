511898e4587392fc67a8ad89e9fdcb812736fe6d
Raj Patel <rajpatel2111@gmail.com>
2017-10-01 08:05:23 -0500

    Added weather

--- HackUTA/weather.py
+++ HackUTA/weather.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import os
 import re
@@ -21,15 +20,17 @@ def weather():
 	number = request.form['From']
 	message_body = request.form['Body']
 	
-	query_extractor = r"From (.*)\nTo (.*)\nVia (.*)\nLeave (.*)?"
+	query_extractor = r"Weather (.*)"
 
 	matches = re.findall(query_extractor, message_body, re.MULTILINE | re.IGNORECASE)
+	
+	origin = matches[0][0]
 
 	darksky_api_key = "d56b7934d5a491dcad4f0d22ef7e839b"
 
 	darksky = pydarksky.DarkSky(darksky_api_key)
 	
-	location = get_location()
+	location = get_location(origin)
 	darksky.latitude = location["lat"]
 	darksky.longitude = location["lng"]
 	
@@ -44,37 +45,15 @@ def weather():
 	return str(resp)
 
 
-def get_location():
+def get_location(origin):
 	maps_api_key = "AIzaSyCEMNKObdTlNK8zdDc2kalZ8y3bRpJo5A0"
 	gmaps = googlemaps.Client(key = maps_api_key)
 	
 	location_geocode = gmaps.geocode(origin)
 	
 	return location_geocode[0]["geometry"]["location"]
-=======
-import os
-import re
-import json
-import requests
-from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
-
-app = Flask(__name__)
-
-
-@app.route('/sms', methods=['GET', 'POST'])
-def weather():
-	msg = request.form['Body']
-	#queryextractor =
-	loc = msg.split()[-1]
-	print(loc)
->>>>>>> 7123adb1791bb2a65069f8c495595b4be078340c
 
 if __name__ == '__main__':
 	app.run(debug=True)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 7123adb1791bb2a65069f8c495595b4be078340c
