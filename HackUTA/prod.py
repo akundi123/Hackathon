b9d9a53e3d66ec987ab4e129814f79333a626d37
Khanguy <khanguy101@gmail.com>
2017-10-01 01:52:58 -0500

    Save for Heroku

--- HackUTA/prod.py
+++ HackUTA/prod.py
@@ -10,7 +10,7 @@ def wolfram():
 	msg = request.form['Body']
 	# EX: https://api.wolframalpha.com/v1/result?i=What+is+the+stock+price+of+Twitter%3F&appid=DEMO
 	# TODO env variable
-	appid = "KLEJ7L-X4Y9P4KLV5"
+	appid = "KLEJ7L-X4Y9P4KLV5" #
 
 	wolfram_url = "http://api.wolframalpha.com/v1/result"
 	response = requests.get(wolfram_url, params={"appid": appid, "i": msg}).content.decode("utf-8")
@@ -20,6 +20,11 @@ def wolfram():
 	out.message(response)
 	return str(out)
 
+
+def maps():
+	#recieves 'directions from {} to {}'
+	# or 		'directions to {} from {}'
+	pass
 
 if __name__ == '__main__':
 	app.run()
