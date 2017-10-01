5e4095ad67c4cf6a3e1633476fa32201905d8293
Raj Patel <rajpatel2111@gmail.com>
2017-09-30 19:05:11 -0500

    Updated Wolfram Alpha

--- HackUTA/wolfram_alpha.py
+++ HackUTA/wolfram_alpha.py
@@ -7,22 +7,26 @@ from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
+
 @app.route('/sms', methods=['GET','POST'])
 def wolfram():
-    #Interact with simple api from wolframalpha
-    number = request.form['From']
-    msg = request.form['Body']
-    #EX: https://api.wolframalpha.com/v1/result?i=What+is+the+stock+price+of+Twitter%3F&appid=DEMO
-    #TODO env variable
-    appid = "KLEJ7L-X4Y9P4KLV5"
-    r = "http://api.wolframalpha.com/v1/result?appid={}&i={}%3f".format(appid,msg.replace(' ','+'))
-    response = requests.get(r).content.decode('utf-8')
-
-    print(response)
-    out = MessagingResponse()
-    out.message(response)
-    return str(out)
+	#Interact with simple api from wolframalpha
+	number = request.form['From']
+	msg = request.form['Body']
+	#EX: https://api.wolframalpha.com/v1/result?i=What+is+the+stock+price+of+Twitter%3F&appid=DEMO
+	#TODO env variable
+	appid = "KLEJ7L-X4Y9P4KLV5"
+	
+	# r = "http://api.wolframalpha.com/v1/result?appid={}&i={}%3f".format(appid,msg.replace(' ','+'))
+	# response = requests.get(r).content.decode('utf-8')
+	wolfram_url = "http://api.wolframalpha.com/v1/result?appid={}&i={}%3f"
+	response = requests.get(wolfram_url, params = {"appid" : appid, "i" : msg}).content.decode("utf-8")
+	
+	print(response)
+	out = MessagingResponse()
+	out.message(response)
+	return str(out)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
 
